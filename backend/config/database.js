07c6f16621b9df22572a27846cf259d838ca51e2
andreinf <pipe.infacor@gmail.com>
2025-11-29 16:27:37 -0500

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -6,7 +6,7 @@ const dbConfig = {
   host: process.env.DB_HOST || 'localhost',
   port: process.env.DB_PORT || 3306,
   user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
+  password: process.env.DB_PASS || '',   // <--- Cambiado de DB_PASSWORD a DB_PASS
   database: process.env.DB_NAME || 'licea_platform',
   connectionLimit: 10,
   charset: 'utf8mb4',
